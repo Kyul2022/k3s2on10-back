4769356b941d9f68bb625dd4fcfeef282d58c5eb
Kyul <106420255+Kyul2022@users.noreply.github.com>
2026-02-16 17:09:26 +0100

    Forgot to import datasources

--- src/test/java/com/example/k3s_2on10/K3s2on10ApplicationTests.java
+++ src/test/java/com/example/k3s_2on10/K3s2on10ApplicationTests.java
@@ -2,6 +2,7 @@ package com.example.k3s_2on10;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootTest(exclude = DataSourceAutoConfiguration.class)
 class K3s2on10ApplicationTests {
